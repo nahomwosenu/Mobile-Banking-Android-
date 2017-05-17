b34d2f7d62d2b6fdc81bef7827db8ed48a6a3667
nahomwosenu <nahometete@yahoo.com>
2017-05-16 19:32:30 -0700

    fixed minor bugs

---
+++ app/src/main/java/g4it/finalproject/mobilebanking/USSDActivity.java
@@ -0,0 +1,13 @@
+package g4it.finalproject.mobilebanking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class USSDActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ussd);
+    }
+}
